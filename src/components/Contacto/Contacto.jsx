9e4878f1cb017945f0b99b501ae706200d7e9c88
LozanoTheStain <adslozanov@gmail.com>
2022-10-25 10:00:22 -0500

    Se ha eliminado, localización y se ha modificado eficazmente la section de contacto

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -6,12 +6,11 @@ const Contacto = () => {
     <div>
       {/* Contacto */}
       <section className="contacto" id="contacto">
-        <div className="contenido seccion">
+        <div className="contenido-seccion">
           <h1 className="title-principal">
             Comunicate con nosotros para conocer mas sobre nosotros y los
             servicios que ofrecemos para ti
           </h1>
-
           <div className="container-items">
             <div className="items">
               <input
@@ -37,34 +36,10 @@ const Contacto = () => {
                 placeholder="Mensaje"
               ></textarea>
               <button>
-                Enviar Mensaje 
+                Enviar Mensaje
                 <i class="fa-solid fa-paper-plane"></i>
                 <span class="overlay"></span>
               </button>
-            </div>
-          </div>
-
-          {/* Localización */}
-          <div class="items">
-            <img
-              src="../assets/Images/Captura de pantalla 2022-10-25 083842.png"
-              alt=""
-            />
-            <div class="info">
-              <ul>
-                <li>
-                  <i class="fa-sharp fa-solid fa-map-location-dot"></i>
-                  Colombia: Medellin Dirección: Cra. 115 # 38 BD 8
-                </li>
-                <li>
-                  <i class="fa-solid fa-mobile"></i>
-                  Contactame: Proximamente
-                </li>
-                <li>
-                  <i class="fa-solid fa-envelopes-bulk"></i>
-                  Email: codigoc13@sonbata.org
-                </li>
-              </ul>
             </div>
           </div>
         </div>
